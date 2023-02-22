6262d30266fc58d0a8ae1727adea39817283a34b
Илья <vitilyusha@gmail.com>
2023-02-22 17:36:47 +0300

    lab5 Shape

--- app/src/main/java/com/example/laba1/MenuActivity.java
+++ app/src/main/java/com/example/laba1/MenuActivity.java
@@ -2,8 +2,15 @@ package com.example.laba1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -11,7 +18,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
